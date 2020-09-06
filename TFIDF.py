5a22c97786efe6b7a9c2ff498143de32a2940286
annam-shivakrishna <58822247+annam-shivakrishna@users.noreply.github.com>
2020-09-06 18:12:33 +0530

    Update TFIDF.py

--- TFIDF.py
+++ TFIDF.py
@@ -15,9 +15,10 @@ documents = ['the the universe has very many stars',
 split_documents = []
 for i,j in enumerate(documents):
     split_documentss = j.split()
+    print(split_documentss)
     split_documents.append(split_documentss)
     split_documents[i] = [(word,split_documentss.count(word)) for word in split_documentss]
-#print(len(documents))    
+    
 
 cleaned_documents_1 = []
 
@@ -25,6 +26,7 @@ cleaned_documents_1 = []
 for k in range(0,len(documents)):
     cleaned_document = []
     for d in split_documents[k]:
+        
         if d not in cleaned_document:
             cleaned_document.append(d)
     cleaned_documents_1.append(cleaned_document)
@@ -32,12 +34,15 @@ for k in range(0,len(documents)):
 normalized_freq1={}
 
 for q in range(0,len(cleaned_documents_1)):
-    sentence = split_documents[q]
+    sentence = cleaned_documents_1[q]
+    print(sentence)
     len_sente = len(sentence)
+    print(len_sente)
     for freq in cleaned_documents_1[q]:
-        first1 = []
         first = float(freq[1]/len_sente)
         normalized_freq1.update({freq[0]:first})
+    
+      
         
 print(len(normalized_freq1))
 
@@ -56,7 +61,6 @@ for t in range(0, len(documents)):
     sentence = cleaned_documents_1[t]
     sum=0
     #print(sentence)
-    
     for each in sentence:
         keywords = each[0]
         cleaned_dup.append(keywords)
@@ -71,15 +75,21 @@ group=df.pivot_table(index=["words"],aggfunc = "size")
 df1 = pd.DataFrame(data=group.to_frame())
 df1["values"] = N/df1
 df1["log_values"]=np.log(df1["values"])
+df1["normalized_freq"]=normalized_freq1.values()
 
 # final TFIDF
 
-print(normalized_freq1.values())
 
-df1["TFIDF_keys"] = normalized_freq1
-df1["TFIDF_values"] = normalized_freq1.values()
+df1["TFIDF_value"]=df1["normalized_freq"]*df1["log_values"]
 
 
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+x = vectorizer.fit_transform(documents)
+m=vectorizer.get_feature_names()
+print(x.shape)
 
 
 
